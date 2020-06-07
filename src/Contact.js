6e96bebf1531f7afb50d36e24b8c9e7395055a86
Alahad Patrawala <41312423+ahadpatrawala@users.noreply.github.com>
2020-06-07 13:43:41 -0700

    Add files via upload

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,60 @@
 import React from 'react';
 import {Helmet} from 'react-helmet'
+import axios from "axios";
+import { Redirect } from 'react-router-dom';
 
-function Contact(){
+const API_PATH = 'https://www.refacenterprises.com/contactus.php';
+
+class Contact extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			name: '',
+			email: '',
+			phone: '',
+			body: '',
+			mailSent: '',
+			error: null
+		}
+	}
+
+	handleSubmit=(e)=>{
+		e.preventDefault();
+		axios({
+			method: "post",
+			url: `${API_PATH}`,
+			headers: { "content-type": "application/json" },
+			data: this.state
+		  })
+			.then(result => {
+				console.log(result)
+			  if (result) {
+				this.setState({
+				  mailSent: result.data.sent
+				},() => console.log(this.state));
+				this.setState({ error: false });
+				window.location.href ="https://www.refacenterprises.com/message";
+			  } else {
+				this.setState({ error: true });
+				window.location.href ="https://www.refacenterprises.com/error";
+			  }
+			})
+			.catch(error => {
+				this.setState({ error: error.message })
+				console.log(error.message)
+				window.location.href ="https://www.refacenterprises.com/error";
+		});
+		};
+	
+
+	handleChange=(e)=>{
+		this.setState({
+			[e.target.name] : e.target.value
+		})
+	}
+
+render(){
     return(
         <div>
 			<Helmet>
@@ -11,25 +64,25 @@ function Contact(){
 	<meta name="description" content="Manufacturer of all kinds of refrigeration, air-conditioning systems and customized fabrication works, HVAC Manufacturers Company in Mumbai, HVAC Exporters Company in Mumbai." />
 	<meta name="keywords" content="Manufacturer of all kinds of refrigeration, air-conditioning systems and customized fabrication works, HVAC Manufacturers Company in Mumbai, HVAC Exporters Company in Mumbai, refrigerator price, refrigerator online, refrigerator working, refrigeration cycle, refrigeration, refrigeration process, refrigeration manufacturers, refrigeration manufacturers in india, refrigeration manufacturing companies india, refrigeration manufacturing companies, refrigeration manufacturing process, amish refrigeration manufacturing company, bharat refrigeration manufacturing company, rapid refrigeration manufacturing company limited, commercial refrigeration manufacturing companies, commercial refrigeration manufacturing, refrigeration compressor manufacturers, duracold refrigeration mfg company, refrigeration manufacturing company in mumbai, air-conditioner, air-conditioning specialities, air-conditioning system, air-conditioning system design manual, air-conditioning system used in hotels, air conditioning system design, Indian IT recruiter, Human resource consultant, Lifts, Elevators, Escalators, IT Services, Information Technology India, IT Manpower India, IT Manpower Mumbai, Head Hunter Mumbai, Head Hunting India, Head Hunting Companies Mumbai, Head Hunting Companies India, construction crew recruitment from India, outsourcing, Personnel Selection and Recruitment, Executive search in India, Indian recruiter, Total HR solutions, Head hunters in India, Pre-employment check ,HR outsourcing, Contractual staffing, Compensation surveys, Nurses recruiter in India, Physician recruiter in India, Oil and gas personnel from India, marine and shipping recruitment, electromechanical project crew, Due Diligence, Mental aptitude tests, Bio-data Mumbai, best recruiting agency in India, certified recruiter , job consultants India, Indian manpower consultants, Indian job consultants, employment agency, job agency, careers, human resource management, human resource consultants, manpower consulting, manpower consultants India, manpower consultant in India, job consulting firm, placement consultants, hr consultants, recruiting" />	
 			</Helmet>
-            <div className="contact">
+    <div className="contact">
     <h2>Contact Us</h2>
     <div className="clearfix"></div>
 		<div className="container">
-		 <form id="form" className="contact_form" action="/contactus.php" method="POST" >
+		 <form id="form" className="contact_form" action="#" method="POST">
 				<div className="message">
 					<div className="col-md-6 col-sm-6 grid_6 c1">
-						<input type="text" id="name" name="name" className="text" placeholder="Name" required pattern="^[a-zA-Z ]+$"/>
-						<input type="email" id="email" name="email" className="text" placeholder="Email" required pattern="\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*"/>
-						<input type="tel" id="phone" name="phone" className="text" placeholder="Phone" required maxLength="10" pattern="^\d+$"/>
+						<input type="text" id="name" name="name" className="text" placeholder="Name" value={this.state.name} onChange={this.handleChange} required pattern="^[a-zA-Z ]+$"/>
+						<input type="email" id="email" name="email" className="text" placeholder="Email" value={this.state.email} onChange={this.handleChange} required pattern="\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*"/>
+						<input type="tel" id="phone" name="phone" className="text" placeholder="Phone" required maxLength="10" value={this.state.phone} onChange={this.handleChange} pattern="^\d+$"/>
 					</div>
 					<div className="col-md-6 col-sm-6 grid_6 c1">
-                                                <textarea id="body" placeholder="Message" name="body" required></textarea>
+						<textarea id="body" placeholder="Message" name="body" value={this.state.body} onChange={this.handleChange} required></textarea>
 					</div>
 					<div className="clearfix"> </div> 
 				
 				</div>
 
-                        <input type="submit" id="submit" className="more_btn" name="submit" value="Submit" onClick=""/>
+                        <input type="submit" className="more_btn" value="Submit" name="Submit" onClick={this.handleSubmit}/>
 		    </form>
                 </div>
 	</div>
@@ -39,6 +92,7 @@ function Contact(){
 	</div>
         </div>
     );
+}
 }
 
 
--- src/contactus.php
+++ src/contactus.php
@@ -1,4 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Disposition, Content-Type, Content-Length, Accept-Encoding");
+header("Content-type:application/json");
+$rest_json = file_get_contents("php://input");
+$_POST = json_decode($rest_json, true);
+
+echo '<script>console.log("Got data")</script>';
+
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -8,12 +18,13 @@ require './email/PHPMailer.php';
 require './email/SMTP.php';
 
 
-if (isset($_POST['submit'])){
+if (isset($_POST['name'])){
     $name= $_POST['name'];
     $email= $_POST['email'];
     $body= $_POST['body'];
-	$phone=$_POST['phone'];
-	
+    $phone=$_POST['phone'];
+    
+    echo '<script>console.log(\"$name\")</script>';
 	
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
@@ -26,7 +37,7 @@ try {
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'rfcentp@gmail.com';                   // SMTP username
     $mail->Password   = 'rfcEntp@#1';                               // SMTP password
-    $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       =587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
@@ -36,18 +47,22 @@ try {
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Message from website';
     $mail->Body    = 'Message from: '.$name.'<br> Phone: '.$phone.' <br> Email: '.$email.'<br> Message: '.$body;
-
+    echo '<script>console.log("reached v1")</script>';
     $mail->send();
-	header('Location:/message');
+    header('Location:/https://www.refacenterprises.com/message');
+    echo '<script>console.log(\"$mail->ErrorInfo\")</script>';
 	}
 	
 catch (Exception $e) {
-	header('Location:/error');
+    header('Location:/https://www.refacenterprises.com/error');
+    echo '<script>console.log(\"$mail->ErrorInfo\")</script>';
+    echo '<script>console.log("Error")</script>';
 	echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 	//$_SESSION['err'] = $mail->ErrorInfo;
 }
 }
 else{
-    echo 'Message not sent';
-	header('Location:/error');
+    header('Location:https://www.refacenterprises.com/error');
+    echo '<script>console.log(\"$mail->ErrorInfo\")</script>';
+    echo '<script>console.log("No data")</script>';
 }
