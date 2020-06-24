dedf4bb9d2ae883b4ef7a28ccb8e523ca714feb2
Alahad Patrawala <41312423+ahadpatrawala@users.noreply.github.com>
2020-06-24 14:03:42 -0700

    Update contactus.php

--- src/contactus.php
+++ src/contactus.php
@@ -33,16 +33,16 @@ try {
     //Server settings
     //$mail->SMTPDebug = 2;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                   // Set the SMTP server to send through
+    $mail->Host       = 'SMTP Server';                   // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'rfcentp@gmail.com';                   // SMTP username
-    $mail->Password   = 'rfcEntp@#1';                               // SMTP password
+    $mail->Username   = 'email';                   // SMTP username
+    $mail->Password   = 'password';                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       =587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
     $mail->setFrom($email, $name);
-    $mail->addAddress('rfcentp@gmail.com', 'Refac Enterprises');     // Add a recipient
+    $mail->addAddress('Receipent', 'Name');     // Add a recipient
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Message from website';
